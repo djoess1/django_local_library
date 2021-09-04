49dde93dfe459c3182022ffa5d2f1eebc77e73fd
admin <Admin@wajdi-Macbook.local>
2021-09-04 17:41:59 +0300

    update with site

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'false'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['still-taiga-42287.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
