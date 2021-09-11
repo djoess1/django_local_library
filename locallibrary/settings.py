12ed680dd17873ba894d68510cc645a183b11e0a
admin <Admin@wajdi-Macbook.local>
2021-09-11 14:40:52 +0300

    update degub= false2

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = ['still-taiga-42287.herokuapp.com', '127.0.0.1']
 
